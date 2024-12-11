44af9fbfa9b8c92d1a5112cd664aae9abca64cd2
doubleshy <152592305+doubleshy@users.noreply.github.com>
2024-12-12 03:03:36 +0800

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,17 @@ module com.example.demo {
     requires javafx.fxml;
 
 
-    opens com.example.demo to javafx.fxml;
+    opens com.example.demo.controller to javafx.fxml;
+    opens com.example.demo.level to javafx.fxml;
+    opens com.example.demo.level.levelview to javafx.fxml;
+    opens com.example.demo.model to javafx.fxml, org.junit.platform.commons;
+    opens com.example.demo.projectile to javafx.fxml;
+    opens com.example.demo.view to javafx.fxml;
+
     exports com.example.demo.controller;
+    exports com.example.demo.level;
+    exports com.example.demo.level.levelview;
+    exports com.example.demo.model;
+    exports com.example.demo.projectile;
+    exports com.example.demo.view;
 }
